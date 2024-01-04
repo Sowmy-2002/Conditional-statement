fec7e53cc1ae75498042117472350d6d0693bad4
Sowmy-2002 <155138296+Sowmy-2002@users.noreply.github.com>
2024-01-04 19:46:10 +0530

    Add files via upload

---
+++ Conditional statement.py
@@ -0,0 +1,9 @@
+Write a Python program to classify a given number into one of three categories: negative, zero, or positive. The program should take an integer input and print out one of the following statements based on the input:
+
+
+
+"The number is negative." if the number is less than zero.
+
+"The number is zero." if the number is exactly zero.
+
+"The number is positive." if the number is more than zero.
---
+++ Letsupgrade.py
@@ -0,0 +1,10 @@
+# Get user input
+number = int(input("Enter a number: "))
+
+# Check and classify the number
+if number < 0:
+    print("The number is negative.")
+elif number == 0:
+    print("The number is zero.")
+else:
+    print("The number is positive.")
